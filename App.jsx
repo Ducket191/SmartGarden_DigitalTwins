71c9867d4045e0154993731c04902bf78ba0ff14
Ducket <dangminhduc1912008@gmail.com>
2025-09-27 10:48:59 +0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
+import "./App.css";
 
+function App() {
+  const [chartTempData, setChartTempData] = useState([]);
+  const [chartHumidData, setChartHumidData] = useState([]);
+  const [chartBrightData, setChartBrightData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://192.168.1.3:5000/api/data");
+        const json = await res.json();
+
+        const now = new Date().toLocaleTimeString();
+        setChartTempData(prev => {
+          const updated = [...prev, { time: now, temp: json.temperature}];
+          return updated.slice(-20);
+        });
+        setChartHumidData(prev => {
+          const updated = [...prev, {time:now, humid: json.humidity}];
+          return updated.slice(-20);
+        });
+        setChartBrightData(prev => {
+          const updated = [...prev, { time: now, brightness: json.brightness}];
+          return updated.slice(-20);
+        })
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+    const interval = setInterval(fetchData, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "30px" }}>
+      <h2>Brightness, Temperature, and Humidity Over Time</h2>
+      <LineChart width={600} height={300} data={chartTempData}>
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="time" />
+        <YAxis domain={['auto', 'auto']} label={{ value: '°C', angle: -90, position: 'insideLeft' }} />
+        <Tooltip />
+        <Line type="monotone" dataKey="temp" stroke="#ff7300" dot={true} />
+      </LineChart>
+      <LineChart width={600} height={300} data={chartHumidData}>
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="time" />
+        <YAxis domain={['auto', 'auto']} label={{ value: '%', angle: -90, position: 'insideLeft' }} />
+        <Tooltip />
+        <Line type="monotone" dataKey="humid" stroke="#ff7300" dot={true} />
+      </LineChart>
+            <LineChart width={600} height={300} data={chartBrightData}>
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="time" />
+        <YAxis domain={['auto', 'auto']} label={{ value: '%', angle: -90, position: 'insideLeft' }} />
+        <Tooltip />
+        <Line type="monotone" dataKey="brightness" stroke="#ff7300" dot={true} />
+      </LineChart>
+    </div>
+
+  );
+}
+
+export default App;
